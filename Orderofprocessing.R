3d291955683b0762edfd34b8c7129bc85769e24f
TeresaWe <31951377+TeresaWe@users.noreply.github.com>
2018-09-14 12:25:59 +0200

    Add files via upload

---
+++ Orderofprocessing.R
@@ -0,0 +1,17 @@
+###order of processing Navon
+############################################
+##medians
+readloopAGLT.R
+AGLT_FunctionsRT.R
+AGLT_RT_loop.R
+#AGLTRTpreprocessing.R 
+SACS_measuresAGLTmedians.R
+Navon_mediananalyses
+
+#########################################
+#old: means, without RT distribution inspection/preprocessing
+readloopAGLT.R
+AGLT_Functions27.09.R
+AGLT_loop27.09function.R
+SACS_measuresAGLT3.R
+
